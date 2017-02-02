b0a28f77ac978dccc530892b623dc6838514f2bf
0koraj06 <0kkraj06@solet>
2017-02-02 13:01:35 +0000

    Change default location from Fernhurst to Litochoro.

--- app/src/main/java/com/example/a0koraj06/mapping/HelloMap.java
+++ app/src/main/java/com/example/a0koraj06/mapping/HelloMap.java
@@ -3,17 +3,22 @@ package com.example.a0koraj06.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
+import android.widget.EditText;
+import android.view.View.onClickListener;
 
-public class HelloMap extends Activity
-{
 
-    MapView mv;
 
-    public void onCreate(Bundle savedInstanceState)
-    {
+
+public class HelloMap extends Activity implements View.onClickListener {
+
+     MapView mv;
+
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -23,10 +28,31 @@ public class HelloMap extends Activity
         Configuration.getInstance().load
                 (this, PreferenceManager.getDefaultSharedPreferences(this));
 
-        mv = (MapView)findViewById(R.id.map1);
+        mv = (MapView) findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
-        mv.getController().setCenter(new GeoPoint(40.1,22.5));
+        mv.getController().setCenter(new GeoPoint(51.3966, -0.1335));
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+
+
     }
-}
+
+
+    public void onClick(View view)
+    {
+
+        EditText latitude = (EditText)findViewById(R.id.latitude);
+        EditText longitude = (EditText)findViewById(R.id.longitude);
+
+    }
+
+
+
+    }
+
+
+

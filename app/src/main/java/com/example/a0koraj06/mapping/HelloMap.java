d72662726ca97275f1981817a826da55dd341c9a
0koraj06 <0koraj06@solent.ac.uk>
2017-02-09 11:00:45 +0000

    Added Second Activity

--- app/src/main/java/com/example/a0koraj06/mapping/HelloMap.java
+++ app/src/main/java/com/example/a0koraj06/mapping/HelloMap.java
@@ -1,22 +1,30 @@
 package com.example.a0koraj06.mapping;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
 import android.widget.EditText;
-import android.view.View.onClickListener;
 
 
 
 
-public class HelloMap extends Activity implements View.onClickListener {
 
-     MapView mv;
+public class HelloMap extends Activity implements OnClickListener {
+
+    MapView mv;
+
 
     public void onCreate(Bundle savedInstanceState) {
 
@@ -32,12 +40,10 @@ public class HelloMap extends Activity implements View.onClickListener {
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
-        mv.getController().setCenter(new GeoPoint(51.3966, -0.1335));
+        mv.getController().setCenter(new GeoPoint(44, 11.5));
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
-
-
 
     }
 
@@ -45,14 +51,60 @@ public class HelloMap extends Activity implements View.onClickListener {
     public void onClick(View view)
     {
 
-        EditText latitude = (EditText)findViewById(R.id.latitude);
-        EditText longitude = (EditText)findViewById(R.id.longitude);
+        EditText et1 = (EditText) findViewById(R.id.latitude);
+        double latitude = Double.parseDouble(et1.getText().toString());
+        EditText et2 = (EditText) findViewById(R.id.longitude);
+        double longitude= Double.parseDouble(et2.getText().toString());
+        mv.getController().setCenter(new GeoPoint(latitude, longitude));
 
     }
 
 
-
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
     }
+
+
+    public boolean onOptionsItemSelected(MenuItem item)
+            // handles user selecting a menu item
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);            // react to the menu item being selected...
+            return true;
+        }
+        return false;
+    }
+
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean cyclemap = extras.getBoolean("com.example.cyclemap");
+                if(cyclemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.CYCLEMAP);
+                }
+                else
+                {
+                    mv.setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
+
+}
 
 
 
